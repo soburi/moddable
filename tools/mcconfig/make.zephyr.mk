03b0ac88460dc318fb899ea1018cfae5db6a68a1
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-10-19 15:37:40 +0900

    Refactor Zephyr CMake source generation

--- tools/mcconfig/make.zephyr.mk
+++ tools/mcconfig/make.zephyr.mk
@@ -49,6 +49,28 @@ MC_GENERATED_SOURCES = \
         $(TMP_DIR)/mc.xs.c \
         $(TMP_DIR)/mc.resources.c
 
+MC_COMPILE_DEFINITIONS = \
+        XS_ARCHIVE=1 \
+        XSPLATFORM=\"xsPlatform.h\"
+
+ifeq ($(strip $(COMMODETTOBITMAPFORMAT)),)
+else
+MC_COMPILE_DEFINITIONS += kCommodettoBitmapFormat=$(COMMODETTOBITMAPFORMAT)
+endif
+
+ifeq ($(strip $(POCOROTATION)),)
+else
+MC_COMPILE_DEFINITIONS += kPocoRotation=$(POCOROTATION)
+endif
+
+ifeq ($(INSTRUMENT),1)
+MC_COMPILE_DEFINITIONS += MODINSTRUMENTATION=1 mxInstrument=1
+endif
+
+ifeq ($(DEBUG),1)
+MC_COMPILE_DEFINITIONS += _DEBUG=1 mxDebug=1
+endif
+
 XS_RUNTIME_SOURCES_LIST = \
 	$(XS_DIR)/sources/xsAll.c \
 	$(XS_DIR)/sources/xsAPI.c \
@@ -123,44 +145,15 @@ run: build
 $(XS_SOURCES): $(TMP_DIR)/mc.xs.c $(TMP_DIR)/mc.resources.c
 	@echo "# generate xs_sources.cmake"
 	@mkdir -p $(dir $@)
-	@echo "set(MODDABLE_MC_XS $(TMP_DIR)/mc.xs.c)" > $@
-	@echo "set(MODDABLE_MC_RESOURCES $(TMP_DIR)/mc.resources.c)" >> $@
-	@printf "set(XS_RUNTIME_SOURCES" >> $@
-	@for file in $(XS_RUNTIME_SOURCES_LIST); do \
-		printf "\n    %s" "$$file" >> $@; \
-		done; \
-	printf "\n)\n" >> $@
-	@printf "set(XS_PLATFORM_SOURCES" >> $@
-	@for file in $(XS_PLATFORM_SOURCES_LIST); do \
-		printf "\n    %s" "$$file" >> $@; \
-		done; \
-	printf "\n)\n" >> $@
-	@printf "set(MC_SOURCES" >> $@
-	@for file in $(MC_GENERATED_SOURCES); do \
-		printf "\n    %s" "$$file" >> $@; \
-		done; \
-	TMP_DIR=$(TMP_DIR) MAKEFILE_PATH=$(lastword $(MAKEFILE_LIST)) python3 -c 'import base64,os,sys; exec(base64.b64decode("aW1wb3J0IG9zLHN5cwoKdG1wX2RpciA9IG9zLmVudmlyb25bIlRNUF9ESVIiXQptYWtlZmlsZV9wYXRoID0gb3MuZW52aXJvblsiTUFLRUZJTEVfUEFUSCJdCnNvdXJjZXMgPSBbXQoKZGVmIGFkZF9jYW5kaWRhdGUoY2FuZGlkYXRlOiBzdHIpIC0+IE5vbmU6CiAgICBpZiBub3QgY2FuZGlkYXRlIG9yIGNhbmRpZGF0ZS5zdGFydHN3aXRoKCIkKCIpOgogICAgICAgIHJldHVybgogICAgaWYgY2FuZGlkYXRlIGluIHNvdXJjZXM6CiAgICAgICAgcmV0dXJuCiAgICBzb3VyY2VzLmFwcGVuZChjYW5kaWRhdGUpCgp3aXRoIG9wZW4obWFrZWZpbGVfcGF0aCwgInIiLCBlbmNvZGluZz0idXRmLTgiKSBhcyBtYWtlZmlsZToKICAgIGZvciByYXcgaW4gbWFrZWZpbGU6CiAgICAgICAgbGluZSA9IHJhdy5zdHJpcCgpCiAgICAgICAgaWYgbm90IGxpbmUgb3IgbGluZS5zdGFydHN3aXRoKCIjIikgb3IgIjoiIG5vdCBpbiBsaW5lOgogICAgICAgICAgICBjb250aW51ZQogICAgICAgIHRhcmdldCwgcmVzdCA9IGxpbmUuc3BsaXQoIjoiLCAxKQogICAgICAgIHRhcmdldCA9IHRhcmdldC5zdHJpcCgpCiAgICAgICAgaWYgbm90ICh0YXJnZXQuc3RhcnRzd2l0aCgiJChUTVBfRElSKS8iKSBvciB0YXJnZXQuc3RhcnRzd2l0aChmInt0bXBfZGlyfS8iKSk6CiAgICAgICAgICAgIGNvbnRpbnVlCiAgICAgICAgcHJlcmVxID0gcmVzdC5zcGxpdCgifCIsIDEpWzBdLnN0cmlwKCkKICAgICAgICBpZiBub3QgcHJlcmVxOgogICAgICAgICAgICBjb250aW51ZQogICAgICAgIGZpcnN0ID0gcHJlcmVxLnNwbGl0KClbMF0KICAgICAgICBpZiBub3QgZmlyc3Q6CiAgICAgICAgICAgIGNvbnRpbnVlCiAgICAgICAgYWRkX2NhbmRpZGF0ZShmaXJzdCkKCnN5cy5zdGRvdXQud3JpdGUoIiIuam9pbigiXG4gICAgIiArIHNvdXJjZSBmb3Igc291cmNlIGluIHNvdXJjZXMpKQ==").decode("utf-8"))' >> $@
-	printf "\n)\n" >> $@
-	@printf "set(MC_INCLUDE_DIRS" >> $@
-	@for dir in $(INCLUDE_DIRS) $(TMP_DIR) $(XS_DIR)/includes $(XS_DIR)/platforms $(XS_DIR)/sources; do \
-	printf "\n    %s" "$$dir" >> $@; \
-	done; \
-	printf "\n)\n" >> $@
-	#@printf "set(MC_COMPILE_DEFINITIONS\n    XS_ARCHIVE=1\n    INCLUDE_XSPLATFORM=1\n    " >> $@
-	@printf "set(MC_COMPILE_DEFINITIONS\n    XS_ARCHIVE=1\n    XSPLATFORM=\"xsPlatform.h\"" >> $@
-	@if [ -n "$(COMMODETTOBITMAPFORMAT)" ]; then \
-	printf "\n    kCommodettoBitmapFormat=%s" "$(COMMODETTOBITMAPFORMAT)" >> $@; \
-	fi
-	@if [ -n "$(POCOROTATION)" ]; then \
-	printf "\n    kPocoRotation=%s" "$(POCOROTATION)" >> $@; \
-	fi
-	@if [ "$(INSTRUMENT)" = "1" ]; then \
-	printf "\n    MODINSTRUMENTATION=1\n    mxInstrument=1" >> $@; \
-	fi
-	@if [ "$(DEBUG)" = "1" ]; then \
-	printf "\n    _DEBUG=1\n    mxDebug=1" >> $@; \
-	fi
-	@printf "\n)\n" >> $@
+	@python3 $(MODDABLE)/tools/mcconfig/zephyr_generate_cmake.py \
+		--output $@ \
+		--tmp-dir $(TMP_DIR) \
+		--makefile $(lastword $(MAKEFILE_LIST)) \
+		$(foreach file,$(XS_RUNTIME_SOURCES_LIST),--runtime $(file)) \
+		$(foreach file,$(XS_PLATFORM_SOURCES_LIST),--platform $(file)) \
+		$(foreach file,$(MC_GENERATED_SOURCES),--mc-source $(file)) \
+		$(foreach define,$(MC_COMPILE_DEFINITIONS),--define $(define)) \
+		$(foreach dir,$(INCLUDE_DIRS) $(TMP_DIR) $(XS_DIR)/includes $(XS_DIR)/platforms $(XS_DIR)/sources,--include $(dir))
 
 $(TMP_DIR)/mc.xs.c: $(MODULES) $(MANIFEST)
 	@echo "# xsl modules"
